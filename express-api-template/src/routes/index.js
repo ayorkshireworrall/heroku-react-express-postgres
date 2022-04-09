b9a89a674bfe2ac8840e53614aa942f2acfe1c37
aworrall <alex.worrall@keytree.co.uk>
2022-04-09 22:23:17 +0100

    use environment variables

--- express-api-template/src/routes/index.js
+++ express-api-template/src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { environmentName } from '../settings'
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Different response to check still working' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: `Hello from ${environmentName}` }));
 
 export default indexRouter;
---
+++ express-api-template/src/settings.js
@@ -0,0 +1,3 @@
+import dotenv from 'dotenv';
+dotenv.config();
+export const environmentName = process.env.ENVIRONMENT_NAME;
