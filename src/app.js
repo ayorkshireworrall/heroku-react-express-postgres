b607522acf4d41690d2c364d463d66978626ddf4
aworrall <alex.worrall@keytree.co.uk>
2022-04-11 21:00:34 +0100

    use cors to allow resource sharing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+  origin: '*'
+}))
 app.use('/v1', indexRouter);
 
 app.use((err, req, res, next) => {
--- src/settings.js
+++ src/settings.js
@@ -3,3 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const environmentName = process.env.ENVIRONMENT_NAME;
 export const connectionString = process.env.CONNECTION_STRING;
+export const frontEndURL = process.env.FRONT_END_URL;
