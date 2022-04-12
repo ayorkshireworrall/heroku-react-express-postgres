1c37725c8f3aa28c03b76b32d6cf3d438fb6f16c
aworrall <alex.worrall@keytree.co.uk>
2022-04-12 20:24:51 +0100

    sequelize heroku connection options change

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -1,4 +1,13 @@
 import { Sequelize } from 'sequelize';
 import { connectionString, environmentName } from '../settings.js';
 
-export const sequelize = environmentName === 'LOCAL' ? new Sequelize(connectionString) : new Sequelize(connectionString, {ssl: {rejectUnauthorized: false}});
+export const sequelize = environmentName === 'LOCAL' ? 
+    new Sequelize(connectionString) : 
+    new Sequelize(connectionString, {
+        dialectOptions: {
+            ssl: {
+                require: true, // This will help you. But you will see nwe error
+                rejectUnauthorized: false // This line will fix new error
+            }
+        }
+    });
