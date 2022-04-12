c6224832352ac2da9be1ddd3e69bdd63a24566c4
aworrall <alex.worrall@keytree.co.uk>
2022-04-12 20:20:30 +0100

    Modify connection properties

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -1,4 +1,4 @@
 import { Sequelize } from 'sequelize';
-import { connectionString } from '../settings.js';
+import { connectionString, environmentName } from '../settings.js';
 
-export const sequelize = new Sequelize(connectionString);
+export const sequelize = environmentName === 'LOCAL' ? new Sequelize(connectionString) : new Sequelize(connectionString, {ssl: {rejectUnauthorized: false}});
